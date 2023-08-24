88f3c0ee0b2a92287337122099df4fada13de6ab
ad.hanif <ad.hanif@outlook.com>
2023-08-25 01:34:53 +0200

    2.8: The Phonebook Step3 completed

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -4,14 +4,18 @@ import "./App.css";
 function App() {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleChange = (e) => {
     setNewName(e.target.value);
   };
 
+  const handleNumber = (e) => {
+    setNewNumber(e.target.value);
+  };
+
   const addPerson = (e) => {
     e.preventDefault();
-
     const findName = persons.find((person) => {
       return person.name === newName;
     });
@@ -21,6 +25,7 @@ function App() {
     }
     const personObj = {
       name: newName,
+      number: newNumber,
     };
     setPersons([...persons, personObj]);
     setNewName("");
@@ -34,6 +39,9 @@ function App() {
           name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
@@ -42,7 +50,7 @@ function App() {
       {persons.map((person) => {
         return (
           <p style={{ margin: "0px" }} key={person.name}>
-            {person.name}
+            {person.name} {person.number}
           </p>
         );
       })}
